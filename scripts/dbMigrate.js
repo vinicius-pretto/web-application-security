48316436994fea96975a64f9818db8a084414178
Vinicius Pretto <vini.prettodasilva@gmail.com>
2020-10-07 10:50:34 -0300

    Remove MD5 to be able to test SQL injection

--- scripts/dbMigrate.js
+++ scripts/dbMigrate.js
@@ -6,7 +6,7 @@ const config = require("../config");
 const CREATE_USERS_TABLE =
   "CREATE TABLE IF NOT EXISTS users (email VARCHAR(128), password TEXT);";
 
-const INSERT_USERS = `INSERT INTO users (email, password) VALUES ('${config.loginEmail}', MD5('${config.loginPassword}'));`;
+const INSERT_USERS = `INSERT INTO users (email, password) VALUES ('${config.loginEmail}', '${config.loginPassword}');`;
 
 async function migrate() {
   const client = new Client({
