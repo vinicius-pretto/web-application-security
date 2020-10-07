1b20bc5d967440b26738ff87483c1ed457e74904
Vinicius Pretto <vini.prettodasilva@gmail.com>
2020-10-07 10:53:30 -0300

    Add users repository

--- src/router/apiRouter.js
+++ src/router/apiRouter.js
@@ -1,20 +1,36 @@
 const express = require("express");
 const HttpStatus = require("http-status");
 const config = require("../../config");
+const UsersRepository = require("../users/usersRepository");
 
 const router = express.Router();
+const usersRepository = new UsersRepository();
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
-  if (email !== config.loginEmail || password !== config.loginPassword) {
-    return res.status(HttpStatus.UNAUTHORIZED).json({
-      message: "Email or password is invalid",
+  try {
+    const user = await usersRepository.findUserByEmailAndPassword(
+      email,
+      password
+    );
+
+    if (!user) {
+      return res.status(HttpStatus.UNAUTHORIZED).json({
+        message: "Email or password is invalid",
+      });
+    }
+
+    res.status(HttpStatus.OK).json({
+      message: "Authenticated",
+    });
+  } catch (error) {
+    console.log("Login failed", error.stack);
+
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      message: "Internal Server Error",
     });
   }
-  res.status(HttpStatus.OK).json({
-    message: "Authenticated",
-  });
 });
 
 module.exports = router;
---
+++ src/users/usersRepository.js
@@ -0,0 +1,24 @@
+const { Client } = require("pg");
+const config = require("../../config");
+
+class UsersRepository {
+  constructor() {
+    this.client = new Client({
+      connectionString: config.postgres.connectionUri,
+    });
+    this.client.connect();
+  }
+
+  async findUserByEmailAndPassword(email, password) {
+    const response = await this.client.query(
+      `SELECT email FROM users 
+        WHERE email='${email}'
+        AND password='${password}'
+        LIMIT 1
+      `
+    );
+    return response.rows[0];
+  }
+}
+
+module.exports = UsersRepository;
